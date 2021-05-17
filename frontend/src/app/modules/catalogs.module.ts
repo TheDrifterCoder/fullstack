a087cf17e674374f68a41da13d7a66f1f1c901c4
walker <aangel_mtz@hotmail.com>
2021-05-17 17:13:55 -0500

    Using materials

--- frontend/src/app/modules/catalogs.module.ts
+++ frontend/src/app/modules/catalogs.module.ts
@@ -7,6 +7,12 @@ import { AuthService } from '../auth/auth.service';
 import { AuthInterceptor } from '../auth/auth.interceptor';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
+// materiales
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 @NgModule({
   declarations: [
     StudentsComponent,
@@ -15,9 +21,13 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
   imports: [
     CatalogsRoutingModule,
     CommonModule,
+    MatDialogModule,
+    MatTableModule,
+    MatPaginatorModule,
+    BrowserAnimationsModule
   ],
-  providers: [AuthService, 
-    {provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true}
+  providers: [AuthService,
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
   ]
 })
 export class CatalogsModule { }
--- frontend/src/app/pages/catalogs/students/students.component.ts
+++ frontend/src/app/pages/catalogs/students/students.component.ts
@@ -1,14 +1,66 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, ViewChild, Component, OnInit, Inject } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { GlobalsService } from 'src/app/config/globals.service';
+import { StudentsService } from 'src/app/services/catalogs/students.service';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-students',
   templateUrl: './students.component.html',
   styleUrls: ['./students.component.scss']
 })
-export class StudentsComponent implements OnInit {
+export class StudentsComponent implements AfterViewInit  {
+  displayedColumns: string[] = ['position', 'name', 'patern_surname', 'matern_surname', 'birth_date', 'gender', 'academic_level', 'email', 'phone'];
+  dataSource = new MatTableDataSource<any>([]);
+  public dataResponse: any;
+  // dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
+  nPages = this.globals.paginator;
 
-  constructor() { }
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+  }
+
+  constructor(public globals: GlobalsService, private studentService: StudentsService, public dialog: MatDialog) { }
 
   ngOnInit(): void {
+    this.getStudents();
+  }
+
+  openDialog() {
+    const dialogRef = this.dialog.open(insertStudent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
+  getStudents(){
+    this.studentService.studentsAll().subscribe(
+      data => {
+        this.dataResponse = data;
+        this.dataSource.data = this.dataResponse.data;
+      }
+    );
+  }
+
+}
+
+
+
+// html para insertar estudiantes
+@Component({
+  selector: 'student-insert',
+  templateUrl: 'studentsInsert.component.html',
+})
+export class insertStudent {
+  constructor(
+    public dialogRef: MatDialogRef<insertStudent>,
+    @Inject(MAT_DIALOG_DATA) public data: '') {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
   }
 }
